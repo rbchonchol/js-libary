4c430e74b97aeafef43db9782ccb8c5f9c0ad3dc
rongon <rongonbairagee@gmail.com>
2020-10-09 22:12:12 +0600

    add update option

--- js/appES6.js
+++ js/appES6.js
@@ -7,7 +7,6 @@ class Book {
 }
 class Display {
     store(book) {
-        console.log(book);
         let books = localStorage.getItem("books");
         let booksObj;
         if (books == null) {
@@ -43,7 +42,7 @@ class Display {
                         <td>${element.type}</td>
                         <td>
                         <button id="${index}" onclick="remove(this.id)" class="btn btn-danger btn-sm">Remove</button>
-                        <button id="${index}" onclick="editNote(this.id)" class="btn btn-success btn-sm">Edit</button>
+                        <button id="${index}" onclick="edit(this.id)" class="btn btn-success btn-sm">Edit</button>
                         </td>
                     </tr>`;
         });
@@ -84,19 +83,81 @@ class Display {
         }, 2000);
 
     }
-    delete(index) {
-        let books = localStorage.getItem("books");
+}
+function edit(index) {
+    let books = localStorage.getItem("books");
+    if (books == null) {
+        booksObj = [];
+    } else {
+        booksObj = JSON.parse(books);
+    }
+
+    let updateNote = booksObj[index];
+    let editHtml = `
+    <div class="card">
+                    <div class="card-header m-0">
+                        <h4 class="m-0">Update your Book</h4>
+                    </div>
+                    <div class="card-body">
+                        <form id="myFrom">
+                            <div class="form-group row">
+                                <label for="bookName" class="col-sm-3  col-form-label">Name</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="bookName" value="${updateNote.name}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="author" class="col-sm-3 col-form-label">Author</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="author" value="${updateNote.author}">
+                                </div>
+                            </div>
+                            <input type="hidden" class="form-control" id="type" value="${updateNote.type}">
+                            <div class="form-group row">
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-info" id="uppdateBtn"> Update Book </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                    `;
+    let entryForm = document.getElementById('entryForm');
+    entryForm.style.display = "none";
+    let editbooksElm = document.getElementById("editor");
+    editbooksElm.innerHTML = editHtml;
+
+    let uppdateBtn = document.getElementById("uppdateBtn");
+    uppdateBtn.addEventListener("click", function (e) {
+        let name   = document.getElementById('bookName').value;
+        let author = document.getElementById('author').value;
+        let type   = document.getElementById('type').value;
+        let books  = localStorage.getItem("books");
         let booksObj;
         if (books == null) {
             booksObj = [];
         } else {
             booksObj = JSON.parse(books);
         }
+        let myObj = {
+            name  : name,
+            author: author,
+            type  : type
+        }
+        booksObj.push(myObj);
+        localStorage.setItem("books", JSON.stringify(booksObj));
+        display = new Display;
+        display.show();
+
         booksObj.splice(index, 1);
         localStorage.setItem("books", JSON.stringify(booksObj));
-        show();
-    }
+        display.show();
+    });
 }
+
+
+
+
 function remove(index) {
     let books = localStorage.getItem("books");
     let booksObj;
