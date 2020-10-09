cff50cbe6133dc03ab170c18a090dc259a454ab2
rongon <rongonbairagee@gmail.com>
2020-10-09 21:00:19 +0600

    add delete

--- js/appES6.js
+++ js/appES6.js
@@ -6,15 +6,53 @@ class Book {
     }
 }
 class Display {
-    add(book) {
-        let bookString = `
-                        <tr>
-                            <td>${book.name}</td>
-                            <td>${book.author}</td>
-                            <td>${book.type}</td>
-                        </tr>`
-        let tbody = document.getElementById('tbody');
-        tbody.innerHTML += bookString
+    store(book) {
+        console.log(book);
+        let books = localStorage.getItem("books");
+        let booksObj;
+        if (books == null) {
+            booksObj = [];
+        } else {
+            booksObj = JSON.parse(books);
+        }
+        let myObj = {
+            name: book.name,
+            author: book.author,
+            type: book.type
+        }
+        booksObj.push(myObj);
+        localStorage.setItem("books", JSON.stringify(booksObj));
+    }
+
+    show() {
+        let books = localStorage.getItem("books");
+
+        let booksObj;
+        if (books == null) {
+            booksObj = [];
+        } else {
+            booksObj = JSON.parse(books);
+        }
+        let html = "";
+        booksObj.forEach(function (element, index) {
+            html += `
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${element.name}</td>
+                        <td>${element.author}</td>
+                        <td>${element.type}</td>
+                        <td>
+                        <button id="${index}" onclick="remove(this.id)" class="btn btn-danger btn-sm">Remove</button>
+                        <button id="${index}" onclick="editNote(this.id)" class="btn btn-success btn-sm">Edit</button>
+                        </td>
+                    </tr>`;
+        });
+        let booksElm = document.getElementById("tbody");
+        if (booksObj.length != 0) {
+            booksElm.innerHTML = html;
+        } else {
+            booksElm.innerHTML = `<p class="text-light">nothing to show</>`;
+        }
     }
 
     validate(book) {
@@ -31,7 +69,7 @@ class Display {
         myForm.reset();
     }
 
-    show(type, getMassege) {
+    userMassege(type, getMassege) {
         let showMassege = ` <div class="alert alert-${type} alert-dismissible fade show" role="alert">
                             <strong> Massege </strong> ${getMassege}
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -46,6 +84,32 @@ class Display {
         }, 2000);
 
     }
+    delete(index) {
+        let books = localStorage.getItem("books");
+        let booksObj;
+        if (books == null) {
+            booksObj = [];
+        } else {
+            booksObj = JSON.parse(books);
+        }
+        booksObj.splice(index, 1);
+        localStorage.setItem("books", JSON.stringify(booksObj));
+        show();
+    }
+}
+function remove(index) {
+    let books = localStorage.getItem("books");
+    let booksObj;
+    if (books == null) {
+        booksObj = [];
+    } else {
+        booksObj = JSON.parse(books);
+    }
+    booksObj.splice(index, 1);
+    localStorage.setItem("books", JSON.stringify(booksObj));
+
+    display = new Display;
+    display.show();
 }
 
 
@@ -54,13 +118,13 @@ myForm.addEventListener('submit', formData);
 
 function formData(e) {
     event.preventDefault();
-    let name          =   document.getElementById('bookName').value;
-    let author        =   document.getElementById('author').value;
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
     let type;
 
-    let novel         =   document.getElementById('novel');
-    let programming   =   document.getElementById('programming');
-    let poem          =   document.getElementById('poem');
+    let novel = document.getElementById('novel');
+    let programming = document.getElementById('programming');
+    let poem = document.getElementById('poem');
 
     if (novel.checked) {
         type = novel.value;
@@ -73,17 +137,19 @@ function formData(e) {
     }
 
     let book = new Book(name, author, type);
-    display  = new Display;
 
+    display = new Display;
     if (display.validate(book)) {
-
-        display.add(book);
+        display.store(book);
+        display.show();
         display.clear();
-        display.show('success','Entry successfull');
+        display.userMassege('success', 'Entry successfull');
 
-    }else{
+    } else {
 
-        display.show('danger','Entry decline');
+        display.userMassege('danger', 'Entry decline');
 
     }
 }
+display = new Display;
+display.show();
